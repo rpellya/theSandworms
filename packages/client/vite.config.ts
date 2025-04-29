665efd4b385428be7fd859a78305369329a97cde
hardsign28 <tz-vladislav@yandex.kz>
2025-04-29 22:07:08 +0500

    [SW-18] Добавил алиасы, поправил сборку yarn build

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import * as path from 'node:path';
 
 dotenv.config();
 
@@ -14,4 +15,9 @@ export default defineConfig({
 		__SERVER_PORT__: process.env.SERVER_PORT,
 	},
 	plugins: [react(), tsconfigPaths()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
 });
