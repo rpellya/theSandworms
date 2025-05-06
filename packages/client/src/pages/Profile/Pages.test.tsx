04446ba4e22fc90691ef5e5f7fe0e8d90759a340
Антоний Гончаров <60212039@lemanapro.ru>
2025-05-06 16:15:30 +0300

    delete pages test

--- packages/client/src/pages/Profile/Pages.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Profile } from './Profile';
-
-const appContent = 'Profile';
-
-test('Example test', async () => {
-    render(<Profile />);
-    expect(screen.getByText(appContent)).toBeDefined();
-});
