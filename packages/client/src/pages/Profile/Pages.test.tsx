8ce4cd689b6de30762c5c0804f49c677389135f3
Антоний Гончаров <60212039@lemanapro.ru>
2025-05-06 16:37:51 +0300

    update pages test

---
+++ packages/client/src/pages/Profile/Pages.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import { Profile } from './Profile';
+import { Provider } from 'react-redux';
+import { store } from '../../store/index';
+import { BrowserRouter } from 'react-router-dom';
+
+test('Profile renders correctly', () => {
+    render(
+        <Provider store={store}>
+            <BrowserRouter>
+                <Profile />
+            </BrowserRouter>
+        </Provider>,
+    );
+    expect(screen.getByText('Profile')).toBeDefined();
+});
