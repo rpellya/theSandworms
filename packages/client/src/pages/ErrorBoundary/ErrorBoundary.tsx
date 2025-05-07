5587cafbcfc392ba3174e704c98d7ee0891c182e
Антоний Гончаров <60212039@lemanapro.ru>
2025-05-07 19:07:37 +0300

    create error boundary

---
+++ packages/client/src/pages/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,40 @@
+// ErrorBoundary.tsx
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+import cls from './ErrorBoundary.module.scss';
+
+interface Props {
+    children: ReactNode;
+}
+
+interface State {
+    hasError: boolean;
+    errorMessage: string | null;
+}
+
+export class ErrorBoundary extends Component<Props, State> {
+    state: State = {
+        hasError: false,
+        errorMessage: null,
+    };
+
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true, errorMessage: error.message };
+    }
+
+    componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+        console.error('Ошибка перехвачена ErrorBoundary:', error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div className={cls.errorBoundary}>
+                    <h1>Что-то пошло не так.</h1>
+                    <p>{this.state.errorMessage}</p>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
