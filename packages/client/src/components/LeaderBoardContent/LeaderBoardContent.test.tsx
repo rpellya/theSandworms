a84f40078ab43cf2925dba23fcdd22241b132de1
Kuusama <kuuusama@gmail.com>
2025-05-23 19:09:54 +0300

    fix(5.11) test fixed

--- packages/client/src/components/LeaderBoardContent/LeaderBoardContent.test.tsx
+++ packages/client/src/components/LeaderBoardContent/LeaderBoardContent.test.tsx
@@ -1,10 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import { LeaderBoardContent } from './LeaderBoardContent';
-import { ILeaders } from './LeaderBoardContent';
+import { Leaders } from './LeaderBoardContent';
 
 const appContent = 'user A';
 
-const mockLeaders: ILeaders = {
+const mockLeaders: Leaders = {
     list: [
         { login: 'user A', highScore: 100500900 },
         { login: 'user B', highScore: 100508 },
