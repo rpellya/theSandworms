9e36b065749858c5cb0a1eb2404a0b8b59ebc6db
Sergey Chernoyarov <kuuusama@gmail.com>
2025-07-28 08:41:44 +0300

    api url changed

--- packages/client/src/consts/baseUrl.ts
+++ packages/client/src/consts/baseUrl.ts
@@ -1,4 +1,2 @@
 export const baseUrl = 'https://ya-praktikum.tech/api/v2';
-export const apiUrl = `http://localhost:${
-	'undefined' != typeof process ? process.env.SERVER_PORT : __S_P__
-}`;
+export const apiUrl = `http://localhost:3001/api`;
